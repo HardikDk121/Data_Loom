4bd962ac79732c99afc64cd68cf46410790200ca
hardikdk121 <hardik.d.k121@gmail.com>
2025-02-26 06:20:50 +0000

    project setup

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-
-# Data_Loom
+# data_loom
---
+++ backend/server.js
@@ -0,0 +1,10 @@
+import express from express;
+
+app = express();
+
+app.get()
+
+app.listen(5000,()=>{
+    console.log("Express server running on PORT:5000")
+});
+
